aac132064fd4e1c38989ce108a4b06c08786fd8f
Zubair Hassan <zubairhassan@Mac-sSE-Zubair-146.local>
2021-07-28 15:11:40 +0500

    mymodule: changes for v0.1.0

---
+++ awesome.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zubairhassan652/awesome/morestrings"
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(quote.Go())
+}
